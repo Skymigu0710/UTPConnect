1d4ff539a5e679c9f9326a9cd881ed54ba00361d
crisNATALY <U22103883@utp.edu.pe>
2024-09-09 17:42:54 -0500

    Subiendo primer avance

---
+++ login_register/src/App.js
@@ -0,0 +1,12 @@
+import LoginRegister from './Componets/LoginRegister/LoginRegister';
+
+
+function App() {
+  return (
+    <div>
+      <LoginRegister />
+    </div>
+  );
+}
+
+export default App;
