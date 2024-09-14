50eab72d9ab29d6c139cf7899fa028aa1aedece3
crisNATALY <U22103883@utp.edu.pe>
2024-09-14 01:07:40 -0500

    Subiendo login-register traducido al español

--- login_register/src/Componets/LoginRegister/LoginRegister.jsx
+++ login_register/src/Componets/LoginRegister/LoginRegister.jsx
@@ -17,49 +17,49 @@ const LoginRegister = () => {
     <div className ={`wrapper ${action}`}>
         <div className="form-box login">
             <form action="">
-                <h1>Login</h1>
+                <h1>Inicio de sesión</h1>
                 <div className="input-box">
-                    <input type="text" placeholder="Username" requiered />
+                    <input type="text" placeholder="Nombre de usuario" requiered />
                     <FaUser className="icon"/>
                 </div>
                 <div className="input-box">
                     <input type="password" 
-                    placeholder="password" requiered />
+                    placeholder="contraseña" requiered />
                     <FaLock className="icon"/>
                 </div>
                 <div className="remember-forgot">
-                    <label> <input type="checkbox"/>Remember me</label>
-                    <a href="#"> Forgot password?</a>
+                    <label> <input type="checkbox"/>recordarme </label>
+                    <a href="#"> ¿Has olvidado tu contraseña?</a>
                 </div>
-                <button type="submit">Login</button>
+                <button type="submit">Ingresar</button>
                 <div className="register-link">
-                    <p>Don't have an account? <a href="#" onClick={registerLink}>Register</a> </p>
+                    <p>¿No tienes una cuenta? <a href="#" onClick={registerLink}>Registrarse</a> </p>
                 </div>
             </form>
         </div>
 
         <div className="form-box register">
             <form action="">
-                <h1>Registration</h1>
+                <h1>Registro</h1>
                 <div className="input-box">
-                    <input type="text" placeholder="Username" requiered />
+                    <input type="text" placeholder="Nombre de usuario" requiered />
                     <FaUser className="icon"/>
                 </div>
                 <div className="input-box">
-                    <input type="email" placeholder="Email" requiered />
+                    <input type="email" placeholder="correo electronico" requiered />
                     <FaEnvelope  className="icon"/>
                 </div>
                 <div className="input-box">
                     <input type="password" 
-                    placeholder="password" requiered />
+                    placeholder="Contraseña" requiered />
                     <FaLock className="icon"/>
                 </div>
                 <div className="remember-forgot">
-                    <label> <input type="checkbox"/>I agree to the terms & condition</label>
+                    <label> <input type="checkbox"/>Acepto los términos y condiciones</label>
                 </div>
-                <button type="submit">Register</button>
+                <button type="submit">Registrarse</button>
                 <div className="register-link">
-                    <p>Already have an arcount? <a href="#" onClick={loginLink}>Login</a> </p>
+                    <p>¿Ya tienes una cuenta? <a href="#" onClick={loginLink}>Iniciar sesión</a> </p>
                 </div>
             </form>
         </div>
