fe778693cbc3bd3ddb3115d0e3da82a6059e7622
Joel-Palomino <U19218695@utp.edu.pe>
2024-09-13 23:40:05 -0500

    Subiendo parte del avance de la vista

--- utpconnect/src/grupos.js
+++ utpconnect/src/grupos.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import "./grupos.css"
 
 export default function Grupos() {
--- utpconnect/src/index.js
+++ utpconnect/src/index.js
@@ -1,32 +1,64 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import "./index.css";
-import gato from "./images/th.jpeg";
 import Menu from "./menu.js"
 import Grupos from "./grupos.js"
+import VentanaPost from "./crearPost.js"
+import MuroPost from "./muroPost.js"
 
-function App() {
-  return (
-    <div className="container">
-      <div className="section static">
-        <Menu />
-      </div>
-      <div className="section scrollable">   
+    const App = () => {
+        const [posts, setPosts] = useState([
+            {
+                id: 1,
+                content: 'Hola a todos',
+                user: {
+                    name: 'Julio Avila',
+                    profilePicture: '',
+                },
+            },
+            {
+                id: 2,
+                content: 'Otro post',
+                user: {
+                    name: 'Angela Zavala',
+                    profilePicture: '',
+                },
+            },
+        ]);
+        //Crea un nuevo usuario
+        const handlePostCreate = (newPostContent) => {
+            const newPost = {
+                id: posts.length + 1,
+                content: newPostContent,
+                user: {
+                    name: 'Joel Palomino',
+                    profilePicture: '',
+                },
+            };
+            setPosts([newPost, ...posts]);// Añade el nuevo post al principio de la lista de posts
+        };
 
-      </div>
-      <div className="section static">
-        <Grupos />
-      </div>
-    </div>
-  )
+        return (
+            <div className="container">
+                <div className="section static">
+                    <Menu />
+                </div>
+                <div className="section scrollable">
+                    <VentanaPost onPostCreate={handlePostCreate} />
+                    <MuroPost posts={posts} />
+                </div>
+                <div className="section static">
+                    <Grupos />
+                </div>
+            </div>
+        )
+    }
 
-}
+    export default App;
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-
+    const root = ReactDOM.createRoot(document.getElementById('root'));
+    root.render(
+        <React.StrictMode>
+            <App />
+        </React.StrictMode>
+    );
