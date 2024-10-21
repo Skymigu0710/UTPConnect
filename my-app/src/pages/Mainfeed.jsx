0380d77a2be7c125bb9f220d3f3ea9f8a3790bcf
crisNATALY <U22103883@utp.edu.pe>
2024-10-21 13:46:15 -0500

    Actualizando Carpetas

--- my-app/src/pages/Mainfeed.jsx
+++ my-app/src/pages/Mainfeed.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styles from "../styles/profile.module.css";
 import Menu from "../components/Menu";
 import Grupos from "../components/Grupos";
@@ -7,25 +7,30 @@ import PostFeed from '../components/PostFeed';
 const gato = '/images/th.jpeg';
 
 export default function App() {
-    /*const [posts, setPosts] = useState([
-        {
-            id: 1,
-            content: 'Hola a todos',
-            user: {
-                name: 'Julio Avila',
-                profilePicture: gato,
-            },
-        },
-        {
-            id: 2,
-            content: 'Otro post',
-            user: {
-                name: 'Angela Zavala',
-                profilePicture: gato,
-            },
-        },
-    ]);*/
-
+ 
+    const [userData, setUserData] = useState(null); // Estado para almacenar los datos del usuario
+    useEffect(() => {
+      // Intenta obtener el token y los detalles del usuario desde localStorage
+      const token = localStorage.getItem('token');
+      const storedUserDetails = localStorage.getItem('userDetails');
+      if (!token) {
+        // Si no hay token, redirige al login o maneja el error
+         router.push('/LoginRegister'); // Descomenta esta línea si necesitas redirigir
+        console.error('No se encontró token, redirigiendo al login');
+        return;
+      }
+      if (storedUserDetails) {
+        // Si hay detalles del usuario almacenados, configúralos en el estado
+        setUserData(JSON.parse(storedUserDetails));
+      } else {
+        console.error('No se encontraron detalles del usuario en localStorage');
+        // Aquí puedes hacer una solicitud al backend si es necesario
+      }
+    }, []);
+    if (!userData) {
+      return <div>Cargando...</div>; // Muestra un mensaje mientras se cargan los datos
+    }
+    
     return (
         <div className={styles.container}>
             <div className={`${styles.section} ${styles.static}`}>
@@ -37,7 +42,7 @@ export default function App() {
                         <input type="search" className="search-input" placeholder="Buscar grupos..." />
                     </div>
                     <div className={styles.feed}>
-                        <PostFeed />
+                        <PostFeed userData={userData}/>
                     </div>
                 </div>
                 <div className={styles.responsfooter}>
